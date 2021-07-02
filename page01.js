fc4e50605ea00e8a0dd27ac4dc7428941d7c05fa
Ubuntu <ubuntu@ip-172-31-10-40.ap-northeast-1.compute.internal>
2021-07-02 15:27:58 +0900

    20210702

--- page01.js
+++ page01.js
@@ -3,6 +3,22 @@ function drawMap() {
 }
 
 function dragTable() {
-  alert("dragTable") ;  
+      // Bテーブル
+     let tableElemB = document.getElementById('car2');
+
+     // tbody要素にtr要素（行）を最後に追加
+     let trElemB = tableElemB.tBodies[0].insertRow(-1);
+
+     // td要素を追加
+     let cellElemB = trElemB.insertCell(0);
+
+     //td要素にテキストを追加
+     cellElemB.appendChild(document.createTextNode('Eさん'));
+
+     // td要素を追加
+     let cellElemB2 = trElemB.insertCell(1);
+
+     //td要素にテキストを追加
+     cellElemB2.appendChild(document.createTextNode('熱田区○○○○'));
 }
 
