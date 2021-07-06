d5cd0ecbc8829549bab3c89ced9bb0444ccc8aa8
Ubuntu <ubuntu@ip-172-31-10-40.ap-northeast-1.compute.internal>
2021-07-06 09:37:52 +0900

    m

--- page01.js
+++ page01.js
@@ -54,17 +54,40 @@ function dragTable() {
        directionsService.route(request,
          function(response,status){
           if (status == google.maps.DirectionsStatus.OK){
-             console.log(response.routes[0].legs[0].distance.text)
-             console.log(response.routes[0].legs[0].duration.text)
-          //   document.getElementById("textarea1").value = 
-          //   response.routes[0].legs[0].distance.text + 
-          //   "  " +
-          //   response.routes[0].legs[0].duration.text; 
-             //
-            //
+             //console.log(response.routes[0].legs[0].distance.text)
+             //console.log(response.routes[0].legs[0].duration.text)
              directionsRenderer.setDirections(response)
           }
        })
+    
+      // 別のルートを表示
+     directionsService2 = new google.maps.DirectionsService();
+     directionsRenderer2 = new google.maps.DirectionsRenderer();
+    
+     directionsRenderer2.setMap(map);
+      var request = {
+          origin: "愛知県名古屋市熱田区桜田町19-18",
+          destination: "愛知県名古屋市熱田区桜田町19-18",
+          waypoints: [
+             { location: "愛知県名古屋市中区金山1丁目17-18" },
+             { location: "愛知県名古屋市瑞穂区桜見町1丁目1-14" },
+           ], 
+          travelMode: google.maps.DirectionsTravelMode.DRIVING,
+          unitSystem: google.maps.DirectionsUnitSystem.METRIC,
+          optimizeWaypoints: true,
+          avoidHighways: false,
+          avoidTolls: false
+      }
+
+       directionsService2.route(request,
+         function(response,status){
+          if (status == google.maps.DirectionsStatus.OK){
+             //console.log(response.routes[0].legs[0].distance.text)
+             //console.log(response.routes[0].legs[0].duration.text)
+             directionsRenderer2.setDirections(response)
+          }
+       })
+    　
 
       //End Function
       }
