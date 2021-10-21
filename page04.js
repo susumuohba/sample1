35bf618590d2caf1251bbe3e27c85e8064c4843d
susumuohba <susumu.ohba@gmail.com>
2021-10-21 18:55:01 +0900

    2116

--- page04.js
+++ page04.js
@@ -79,33 +79,33 @@
     
     
     
-     directionsRenderer2.setMap(map);
-      var request = {
-          origin: "愛知県名古屋市熱田区桜田町19-18",
-          destination: "愛知県名古屋市熱田区桜田町19-18",
-          waypoints: [
-             { location: "愛知県名古屋市中区金山1丁目17-18" },
-             { location: "愛知県名古屋市瑞穂区桜見町1丁目1-14" },
-           ], 
-          travelMode: google.maps.DirectionsTravelMode.DRIVING,
-          unitSystem: google.maps.DirectionsUnitSystem.METRIC,
-          optimizeWaypoints: true,
-          avoidHighways: false,
-          avoidTolls: false
-      }
+ /////    directionsRenderer2.setMap(map);
+ /////     var request = {
+ /////         origin: "愛知県名古屋市熱田区桜田町19-18",
+/////          destination: "愛知県名古屋市熱田区桜田町19-18",
+ /////         waypoints: [
+ /////            { location: "愛知県名古屋市中区金山1丁目17-18" },
+/////             { location: "愛知県名古屋市瑞穂区桜見町1丁目1-14" },
+ /////          ], 
+ /////         travelMode: google.maps.DirectionsTravelMode.DRIVING,
+ /////         unitSystem: google.maps.DirectionsUnitSystem.METRIC,
+ /////         optimizeWaypoints: true,
+ /////         avoidHighways: false,
+ /////         avoidTolls: false
+ /////     }
 
-       directionsService2.route(request,
-         function(response,status){
-          if (status == google.maps.DirectionsStatus.OK){
+ /////      directionsService2.route(request,
+ /////        function(response,status){
+ /////         if (status == google.maps.DirectionsStatus.OK){
              //console.log(response.routes[0].legs[0].distance.text)
              //console.log(response.routes[0].legs[0].duration.text)
-             directionsRenderer2.setDirections(response)
-          }
-       })
+  /////           directionsRenderer2.setDirections(response)
+  /////        }
+ /////      })
     　
 
       //End Function
-      }
+  }
 
      
 function page03s01() {
