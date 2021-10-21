3818f6a42274b22e73e1aedae89890fa03392eb0
susumuohba <susumu.ohba@gmail.com>
2021-10-22 07:26:10 +0900

    2201

--- page04.js
+++ page04.js
@@ -1,28 +1,21 @@
-     // Initialize and add the map
-
      let directionsService ;
      let directionsRenderer ;
 
-     let map ; 
-     let listener1 ; 
-
-     // クリックした位置を保存する変数
-     let g_lat_lng ; 
-
-     function initMap() {
-        // The location of Uluru
+     
+　    function initMap() {
+      // The location of Uluru
       /////  const uluru = { lat: 35.172808, lng: 136.89019  };
         // The map, centered at Uluru
       /////   map = new google.maps.Map(document.getElementById("map"), {
       /////    zoom: 15,
       /////    center: uluru,
       /////  });
-        // ダミーデータの描画
-        test1()
+      // 経路の計算
+        CaclRoutes()
       }
 
       // Button Event
-      function test1() {
+      function CaclRoutes() {
   
       directionsService = new google.maps.DirectionsService();
       directionsRenderer = new google.maps.DirectionsRenderer();
@@ -46,19 +39,12 @@
        directionsService.route(request,
          function(response,status){
           if (status == google.maps.DirectionsStatus.OK){
-            console.log(response.routes.length)
-            
-            //for (let i = 0 ; i < response.routes[0].legs.lengt ; i++){
-            //  console.log(response.routes[0].legs[i].distance.text)
-            //  console.log(response.routes[0].legs[i].duration.text)
-            //}
 
-            console.log(response.routes[0].legs.length)
-
-            console.log(response.routes[0].legs)
-
+            // 中継地点の回る順番が入る配列
             console.log(response.routes[0].waypoint_order)
-            
+           
+            // 各経路（出発=>中継地点１など）の情報が入る配列
+            console.log(response.routes[0].legs)
             
           }
        })
@@ -67,23 +53,3 @@
   }
 
      
-function page03s01() {
-  navigator.geolocation.getCurrentPosition(page03s02);   
-}
-
-
-function page03s02() {
-  let geo_text = "緯度:" + position.coords.latitude + "\n";
-    geo_text += "経度:" + position.coords.longitude + "\n";
-    geo_text += "高度:" + position.coords.altitude + "\n";
-    geo_text += "位置精度:" + position.coords.accuracy + "\n";
-    geo_text += "高度精度:" + position.coords.altitudeAccuracy  + "\n";
-    geo_text += "移動方向:" + position.coords.heading + "\n";
-    geo_text += "速度:" + position.coords.speed + "\n";
-
-    // let date = new Date(position.timestamp);
-
-    //geo_text += "取得時刻:" + date.toLocaleString() + "\n";
-
-    alert(geo_text);
-}
