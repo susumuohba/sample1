984e59d880bd37ab74640c49131a9fe2bbe44de5
susumuohba <susumu.ohba@gmail.com>
2021-10-25 16:13:38 +0900

    2511

--- page04.js
+++ page04.js
@@ -15,11 +15,11 @@
       // 経路の計算
       //ダミーのデータ
       let origindestination = {lat:35.142589 , lng:136.909671} ; 
-      
+    
       let customers = [
-        {id:1 , lat:35.17082365 , lng:136.93391909},
-        {id:2 , lat:35.173805 , lng:136.879495} , 
-        {id:3 , lat:35.176345 , lng:136.907854}
+        {id:1 , lat:35.17082365 , lng:136.93391909, order, estimatetime},
+        {id:2 , lat:35.173805 , lng:136.879495    ,order, estimatetime} , 
+        {id:3 , lat:35.176345 , lng:136.907854    ,order, estimatetime}
       ] ;
       
       //
@@ -58,7 +58,19 @@
           if (status == google.maps.DirectionsStatus.OK){
 
             // 中継地点の回る順番が入る配列
-            console.log(response.routes[0].waypoint_order)
+            // 順番をセットする
+            // response.routes[0].waypoint_orderには、順番が逆順（大きい方が優先）
+            // で入っているため、反転する
+            let len =  response.routes[0].waypoint_order.length -1 ; 
+
+            for(let i in response.routes[0].waypoint_order) {
+              customers[i].order  = len -  parseInt(response.routes[0].waypoint_order[i]);
+              //
+              console.log(customers[i].order) ;
+            }
+            
+            
+            //console.log(response.routes[0].waypoint_order)
            
             // 各経路（出発=>中継地点１など）の情報が入る配列
             console.log(response.routes[0].legs)
