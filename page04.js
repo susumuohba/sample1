a88ddaa0fd1097392770caf9c49a95d3ec107899
susumuohba <susumu.ohba@gmail.com>
2021-10-25 15:17:51 +0900

    2507

--- page04.js
+++ page04.js
@@ -13,32 +13,41 @@
       // 経路の計算
       //ダミーのデータ
       let origindestination = {lat:35.142589 , lng:136.909671} ; 
-      let customer1  = {id:1 , lat:35.17082365 , lng:136.93391909} ;
-      let customer2  = {id:1 , lat:35.173805 , lng:136.879495} ;
-      let customer3  = {id:1 , lat:35.176345 , lng:136.907854} ;
+      
+      let customers = [
+        {id:1 , lat:35.17082365 , lng:136.93391909},
+        {id:2 , lat:35.173805 , lng:136.879495} , 
+        {id:1 , lat:35.176345 , lng:136.907854}
+      ] ;
       
       //
-        CaclRoutes(origindestination)
+        CaclRoutes(origindestination, customers)
       }
 
       // 引数の1番目は、スタートとゴールの座標
-      function CaclRoutes(origindestination) {
+      function CaclRoutes(origindestination, customers) {
   
       directionsService = new google.maps.DirectionsService();
       directionsRenderer = new google.maps.DirectionsRenderer();
 
-      //
-      let waypointsString = [{ location:  new google.maps.LatLng(35.17082365 ,136.93391909)}]
+      //  
+      let waypointsObject
+
+      for (let i  in customers ){
+        waypointsObject.push({ location:  new google.maps.LatLng(35.17082365 ,136.93391909)}) ; 
+      }
+      // 
+      //let waypointsObject = [{ location:  new google.maps.LatLng(35.17082365 ,136.93391909)}]
 
       //  { location:  new google.maps.LatLng(35.173805 ,136.879495) },
       //  { location:  new google.maps.LatLng(35.176345 ,136.907854)}," 
 
       
       let request = {
-          //origin: new google.maps.LatLng(35.142589 ,136.909671),
+          
           origin: new google.maps.LatLng(parseFloat(origindestination.lat) ,parseFloat(origindestination.lng)),
           destination: new google.maps.LatLng(parseFloat(origindestination.lat) ,parseFloat(origindestination.lng)),
-          waypoints:waypointsString , 
+          waypoints:waypointsObject , 
  //         waypoints: [
  //            { location:  new google.maps.LatLng(35.17082365 ,136.93391909)},
  //            { location:  new google.maps.LatLng(35.173805 ,136.879495) },
